92e72b33e91efae2b7934e2eea62d102d56665ac
unknown <kohli6010@gmail.com>
2019-07-22 12:52:16 +0530

    Updates

---
+++ precache-manifest.800bd378971919bb35a223ef4d109ae7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542f4368e72dab383aba795017d3192b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9c198d941528b4ddbcee",
+    "url": "/monsters-rolodex/static/css/main.4c17dcf2.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/monsters-rolodex/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "9c198d941528b4ddbcee",
+    "url": "/monsters-rolodex/static/js/main.021e9cb6.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
